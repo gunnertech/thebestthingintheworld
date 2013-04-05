9f83abb447708e90748c9cc7dc0de9f397090f68
Dary Merckens <dary@gunnertech.com>
2013-04-04 23:54:58 -0400

    create tags

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -36,6 +36,16 @@ class Thing < ActiveRecord::Base
     User.find(versions.first.whodunnit)
   end
   
+  def tag_list
+    tags.map(&:name).join(', ')
+  end
+  
+  def tag_list=(names)
+    self.tags = names.split(",").map do |n|
+      Tag.where(name: n.strip).first_or_create!
+    end
+  end
+  
   def add_assigned_things
     User.find_in_batches do |group|
       sleep(1)
