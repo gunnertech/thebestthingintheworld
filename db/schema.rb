f4ff26450b1f8e278fcd611415298f127fced4c3
Cody Swann <cody@gunnertech.com>
2013-07-25 19:57:30 -0400

    minor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130606234747) do
+ActiveRecord::Schema.define(:version => 20130725235626) do
 
   create_table "assigned_things", :force => true do |t|
     t.integer  "thing_id"
@@ -110,25 +110,26 @@ ActiveRecord::Schema.define(:version => 20130606234747) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                       :default => "",   :null => false
-    t.string   "encrypted_password",          :default => "",   :null => false
+    t.string   "email",                                    :default => "",   :null => false
+    t.string   "encrypted_password",                       :default => "",   :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",               :default => 0
+    t.integer  "sign_in_count",                            :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                    :null => false
-    t.datetime "updated_at",                                    :null => false
+    t.datetime "created_at",                                                 :null => false
+    t.datetime "updated_at",                                                 :null => false
     t.string   "name"
-    t.boolean  "send_new_thing_notification", :default => true, :null => false
+    t.boolean  "send_new_thing_notification",              :default => true, :null => false
     t.string   "facebook_access_token"
-    t.integer  "facebook_id"
+    t.integer  "facebook_id",                 :limit => 8
     t.string   "twitter_access_token"
     t.string   "twitter_access_secret"
     t.integer  "twitter_id"
+    t.datetime "oauth_expires_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
